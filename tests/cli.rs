53ff14d2b8c424741a1a3bbd6b427bfb9ec5b7db
Robin Park <robin.j.park@gmail.com>
2020-11-15 18:31:48 -0500

    Add more unit tests of entire mtie binary
    Add tests for the '--version' and '--help' options

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,13 +3,38 @@ use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
 #[test]
-fn file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
+fn test_file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("mtie")?;
 
     cmd.arg("--input").arg("test/file/does/not/exist");
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn test_version() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("mtie")?;
+
+    cmd.arg("--version");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("mtie")?;
+
+    cmd.arg("--help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("USAGE"))
+        .stdout(predicate::str::contains("Robin Park"))
+        .stdout(predicate::str::contains("robin.j.park@gmail.com"))
+        .stdout(predicate::str::contains("Calculates MTIE from a series of TIE input data."));
 
     Ok(())
 }
