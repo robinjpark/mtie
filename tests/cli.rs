e637f2d4053f645ffc3672850e2eafafa013fcf9
Robin Park <robin.j.park@gmail.com>
2020-11-15 18:53:58 -0500

    Add unit tests of application, processing TIE data
    Add unit tests of application's processing of TIE data from
    a file, including:
    - test valid data is processed properly, and output is correct
    - test invalid data is mentioned, but application still works.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,9 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
+use std::io::Write;
+use std::fs::File;
 use std::process::Command; // Run programs
+use tempfile::tempdir;
 
 #[test]
 fn test_file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -39,3 +42,62 @@ fn test_help() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+#[test]
+fn test_mtie_from_file() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = tempdir()?;
+    let tmp_file_path = tmp_dir.path().join("tie");
+    let mut tmp_file = File::create(&tmp_file_path)?;
+    writeln!(tmp_file, "1.0")?;
+    writeln!(tmp_file, "2.1")?;
+    writeln!(tmp_file, "3.2")?;
+
+    let mut cmd = Command::cargo_bin("mtie")?;
+    cmd.arg("--input")
+        .arg(tmp_file_path);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("1.1"))
+        .stdout(predicate::str::contains("2.2"));
+
+    Ok(())
+}
+
+#[test]
+fn test_bad_data_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = tempdir()?;
+    let tmp_file_path = tmp_dir.path().join("tie");
+    let mut tmp_file = File::create(&tmp_file_path)?;
+    writeln!(tmp_file, "1.0")?;
+    writeln!(tmp_file, "this_is_not_a_number")?;
+    writeln!(tmp_file, "2.1")?;
+
+    let mut cmd = Command::cargo_bin("mtie")?;
+    cmd.arg("--input")
+        .arg(tmp_file_path);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("1.1"))
+        .stderr(predicate::str::contains("Ignoring line 2 'this_is_not_a_number': it does not contain a valid number"));
+
+    Ok(())
+}
+
+#[test]
+fn test_comments_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = tempdir()?;
+    let tmp_file_path = tmp_dir.path().join("tie");
+    let mut tmp_file = File::create(&tmp_file_path)?;
+    writeln!(tmp_file, "1.0")?;
+    writeln!(tmp_file, "# This is a comment")?;
+    writeln!(tmp_file, "2.1")?;
+
+    let mut cmd = Command::cargo_bin("mtie")?;
+    cmd.arg("--input")
+        .arg(tmp_file_path);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("1.1"))
+        .stderr(predicate::str::is_empty());
+
+    Ok(())
+}
