de8492bd167ab215934eb232c340f6593e5b0de2
Robin Park <robin.j.park@gmail.com>
2020-11-16 21:10:32 -0500

    Remove misleading comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 //! The binary crate.
 //! It does nothing except call into the library.
+
 extern crate mtielib;
 
-/// The main entry point of the binary.
-///
-/// It simply calls the main() function in the associated library.
 fn main() {
     if let Err(err) = mtielib::run() {
         eprintln!("Error: {:?}", err);
